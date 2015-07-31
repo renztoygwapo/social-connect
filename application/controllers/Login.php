15c502f5e871b2735fe79724336e31212773687d
Renz <renztoygwapo@gmail.com>
2015-07-31 14:06:20 +0800

    add google auth

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -25,10 +25,10 @@ class Login extends CI_Controller {
 
 
      // Google OAuth Crediential 
-        private $client_id = '594982528503-v8cvpv5v5mhpmquetjcbqbrh4rgk42ii.apps.googleusercontent.com';
-        private $client_secret = '_9ZPSUpKyCpfBuiGQH8c7nFM';
-        private $redirect_uri = 'http://mytutorshack.local.com/login/google_login';
-        private $simple_api_key = 'AIzaSyCCuDJXaLey8aWJL0yW_Z0TyqgIhQ044xw';
+        private $client_id = '1009995461410-dccqpo5pmm1mql4n1341cvuimulenhim.apps.googleusercontent.com';
+        private $client_secret = 'zFqWA0S9bDVWZ-gSgQeVmQOM';
+        private $redirect_uri = 'http://socialconnect.dev/login/google_login';
+        private $simple_api_key = 'AIzaSyBXMsd7udQFoybxuWzYRxSIHNAWZmuEuL0';
 
     function __construct() {
         parent::__construct();
@@ -110,8 +110,103 @@ class Login extends CI_Controller {
         //var_dump($data);
         return $data;
       
-    
+   
+ }
+
+    public function google_login(){
+        
+        $data = $this->google_oauth();
+                    
+           if(isset($data['authUrl'])){
+          header('Location:' .$data['authUrl']);
+        } 
+  
+        //get google oauth userdata
+        if (isset($data['userData'])) {
+
+            $user_name = $data['userData']->given_name . " " . $data['userData']->family_name;
+            $user_email = $data['userData']->email;
+            $user_picture = $data['userData']->picture;
+            $user_google_data = array(
+                'user_picture' => $user_picture,
+                'user_name' => $user_name,
+                'user_email' => $user_email
+            );
+
+            //checking email id exists or not in database   
+            $data = $this->login_m->user_email_check($user_google_data['user_email']);
+            isset($data[0]) ? $check_email = trim(strtolower($data[0]->user_email)) : $check_email = '';
+
+            //if not exists then save on db
+            if ($check_email != $user_google_data['user_email']) {
+
+                $insert_google_data = array(
+                    'user_name' => $user_name,
+                    'user_email' => $user_email,
+                     'is_active' => 1
+
+                );
+                // Sucess New User Registration 
+                $new_user = $this->login_m->new_user_registration($insert_google_data);
+            }
+            //set user data to session
+            $this->session->set_userdata($user_google_data);
+            $this->session->all_userdata();
+            $this->home();
+        }
+        
     }
+
+    //Google OAuth Function
+    public function google_oauth() {
+
+        // Include google-php-client library in controller
+        include_once APPPATH . "libraries/google-api-php-client-master/src/Google/Client.php";
+        include_once APPPATH . "libraries/google-api-php-client-master/src/Google/Service/Oauth2.php";
+
+        // Store values in variables from project created in Google Developer Console
+        $client_id = $this->client_id;
+        $client_secret = $this->client_secret;
+        $redirect_uri = $this->redirect_uri;
+        $simple_api_key = $this->simple_api_key;
+
+        // Create Client Request to access Google API
+        $client = new Google_Client();
+        $client->setApplicationName("PHP Google OAuth Login Example");
+        $client->setClientId($client_id);
+        $client->setClientSecret($client_secret);
+        $client->setRedirectUri($redirect_uri);
+        $client->setDeveloperKey($simple_api_key);
+        $client->addScope("https://www.googleapis.com/auth/userinfo.email");
+
+        // Send Client Request
+        $objOAuthService = new Google_Service_Oauth2($client);
+
+        // Add Access Token to Session
+        if (isset($_GET['code'])) {
+            $client->authenticate($_GET['code']);
+            $_SESSION['access_token'] = $client->getAccessToken();
+            header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+        }
+
+        // Set Access Token to make Request
+        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+            $client->setAccessToken($_SESSION['access_token']);
+        }
+
+        // Get User Data from Google and store them in $data
+        if ($client->getAccessToken()) {
+            $userData = $objOAuthService->userinfo->get();
+            $data['userData'] = $userData;
+            $_SESSION['access_token'] = $client->getAccessToken();
+        } else {
+            $authUrl = $client->createAuthUrl();
+            $data['authUrl'] = $authUrl;
+        }
+
+        return $data;    
+    }
+
 	 //loding home view
     public function home() {
         $this->back_browser();
